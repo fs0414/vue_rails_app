d8ba88dbf09b5907658705ad571e423b00409994
soramar <[fujitanisora0414@i.softbank.jp]>
2021-08-18 23:17:32 +0900

    Add: モーダルウィンドウの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_111315) do
+ActiveRecord::Schema.define(version: 2021_08_18_114359) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
 end
