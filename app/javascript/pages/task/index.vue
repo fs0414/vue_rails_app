065d924f5936e1f74919583799c874dcf02d1dc7
soramar <[fujitanisora0414@i.softbank.jp]>
2021-08-20 01:50:59 +0900

    siteikasyono

--- app/javascript/pages/task/index.vue
+++ app/javascript/pages/task/index.vue
@@ -60,7 +60,7 @@ export default {
 </script>
 
 <style scoped>
-  .fade-enter-active, .fade-leave-active {
+.fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
 }
 .fade-enter, .fade-leave-to {
